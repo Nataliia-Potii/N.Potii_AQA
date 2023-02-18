661286a355a14052b856156ce86b7abd2c06a690
Nataliia Potii <nataliapotiy@gmail.com>
2023-02-19 01:30:29 +0200

    Task 5: tests for testing the database

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from modules.api.clients.github import GitHub
+from modules.common.database import Database
 
 
 class User:
@@ -32,4 +33,9 @@ def github_api():
     api = GitHub()
 
     yield api
-    
+
+
+@pytest.fixture
+def database():
+    database = Database()
+    yield database
