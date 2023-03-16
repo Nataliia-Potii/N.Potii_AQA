19848712e993709ae94e21ab882c362a07c06ce9
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-16 03:59:48 +0200

    Task 3: docstrings were added to HTTP protocol tests

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -4,12 +4,19 @@ import requests
 
 @pytest.mark.http
 def test_first_request():
+    """The test sends an HTTP request (the GET method) to the address of a server.
+    It stores the response in a variable.
+    The test displays the server response using f-strings."""
     r = requests.get('https://api.github.com/zen')
     print((f"Response is {r.text}").encode("utf-8"))
 
 
 @ pytest.mark.http
 def test_second_request():
+    """The test sends an HTTP request (the GET method) to the address.
+    To check that the name attribute in response is 'Chris Wanstrath'.
+    To check that the status code of the response is 200
+    To check that the server header of the response is 'GitHub.com'."""
     r = requests.get('https://api.github.com/users/defunkt')
     body = r.json()
     headers = r.headers
@@ -21,6 +28,8 @@ def test_second_request():
 
 @ pytest.mark.http
 def test_status_code_request():
+    """The test sends an HTTP request (the GET method) to the address.
+    To check that the status code of the response is 404"""
     r = requests.get('https://api.github.com/users/Nataliia_Potii')
 
     assert r.status_code == 404
