c243ed33245d89e2332ef7ec89a7a14601ea31f0
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-16 03:49:34 +0200

    Task 2: docstrings were added

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,15 +3,19 @@ import pytest
 
 @pytest.mark.change
 def test_remove_name(user):
+    """The test changes the name to an empty string.
+    To verify that the changes name field is empty as expected."""
     user.name = ''
     assert user.name == ''
 
 
 @pytest.mark.check
 def test_name(user):
+    """To check that the user name field has expected value - 'Nataliia'."""
     assert user.name == 'Nataliia'
 
 
 @pytest.mark.check
 def test_second_name(user):
+    """To check that the user second name field has value - 'Potii'."""
     assert user.second_name == 'Potii'
--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -3,9 +3,11 @@ import pytest
 
 @pytest.mark.check
 def test_change_name(user):
+    """The check that the name is 'Nataliia'."""
     assert user.name == 'Nataliia'
 
 
 @pytest.mark.check
 def test_change_second_name(user):
+    """To check that the second name is 'Potii'."""
     assert user.second_name == 'Potii'
