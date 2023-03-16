4c5fca31a708577231715842ec1be88a6f175abc
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-16 03:11:46 +0200

    Task 4: docstrings were added

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -2,13 +2,16 @@ import requests
 
 
 class GitHub:
+    """The class describes the client API which allows to work with GitHub"""
     def get_user(self, username):
+        """The method send request get using parameter username to find an user"""
         r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
         return body
 
     def search_repo(self, name):
+        """The method send request get using parameter name to search repositories"""
         r = requests.get(
             "https://api.github.com/search/repositories",
             params={"q": name},
