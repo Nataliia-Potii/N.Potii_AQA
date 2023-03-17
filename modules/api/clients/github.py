75af64d8d3c2ea3f57beeafd8bf3f392948548ee
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-17 05:58:00 +0200

    Task 4: minor changes to docstrings

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -2,16 +2,23 @@ import requests
 
 
 class GitHub:
-    """The class describes the client API which allows to work with GitHub"""
+    """
+    Created class GitHub describes the client API which allows to work with GitHub.
+    """
+
     def get_user(self, username):
-        """The method send request get using parameter username to find an user"""
+        """
+        The method send get request using parameter username to find an user.
+        """
         r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
         return body
 
     def search_repo(self, name):
-        """The method send request get using parameter name to search repositories"""
+        """
+        The method send get request using parameter name to search repositories.
+        """
         r = requests.get(
             "https://api.github.com/search/repositories",
             params={"q": name},
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -3,21 +3,27 @@ import pytest
 
 @pytest.mark.api
 def test_user_exists(github_api):
-    """The test created to check that user with this username exists on Github"""
+    """
+    The test created to check that user with this username exists on Github.
+    """
     user = github_api.get_user('defunkt')
     assert user['login'] == 'defunkt'
 
 
 @pytest.mark.api
 def test_user_non_exists(github_api):
-    """The user does not exist on Github"""
+    """
+    The user does not exist on Github.
+    """
     r = github_api.get_user('butenkosergii')
     assert r['message'] == 'Not Found'
 
 
 @pytest.mark.api
 def test_repo_can_be_found(github_api):
-    """Repositories can be found on Github"""
+    """
+    Repositories can be found on Github.
+    """
     r = github_api.search_repo('become-qa-auto')
     assert r['total_count'] == 37
     assert 'become-qa-auto' in r['items'][0]['name']
@@ -25,6 +31,8 @@ def test_repo_can_be_found(github_api):
 
 @pytest.mark.api
 def test_repo_cannot_be_found(github_api):
-    """The repository doesn't exist on Github"""
+    """
+    The repository doesn't exist on Github.
+    """
     r = github_api.search_repo('sergiibutenko_repo_non_exist')
     assert r['total_count'] == 0
