832f849ff7ed76cbb3406bc5a938b2c66fca3ae6
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-17 06:03:45 +0200

    Task 5: changes to docstrings

--- modules/common/database.py
+++ modules/common/database.py
@@ -2,72 +2,93 @@ import sqlite3
 
 
 class Database():
-    """The class allows to interact with the database"""
+    """
+    The class Database was created to interact with the database.
+    """
+
     def __init__(self):
-        """The class constructor where two attributes are initialized.
+        """
+        The class constructor where two attributes are initialized.
         The connection allows to interract with the db.
-        The cursor is an entity that can execute commands in the db"""
+        The cursor is an entity that can execute commands in the db.
+        """
         self.connection = sqlite3.connect(
-            r'C:\Users\NP\N.Potii_AQA' + r'\become_qa_auto.db')
+            r'C:\Users\lorel\N.Potii_AQA' + r'\become_qa_auto.db')
         self.cursor = self.connection.cursor()
 
     def test_connection(self):
-        """The method executes the SQL query SELECT sqlite_version().
-        After that the version of the db is shown in the terminal."""
+        """
+        The method executes the SQL query SELECT sqlite_version().
+        After that the version of the db is shown in the terminal.
+        """
         sqlite_select_Query = "SELECT sqlite_version();"
         self.cursor.execute(sqlite_select_Query)
         record = self.cursor.fetchall()
         print(f"Connected successfully. SQLite Database Version is: {record}")
 
     def get_all_users(self):
-        """The method returns the fields values: name, address, city for all users from customers"""
+        """
+        The method returns the fields values: name, address, city for all users from customers.
+        """
         query = "SELECT name, address, city From customers"
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
 
     def get_user_address_by_name(self, name):
-        """The method has one required parameter - name.
-        It returns the values of this fields from table for the user with the specific name"""
+        """
+        The method has one required parameter - name.
+        It returns the values of this fields from table for the user with the specific name.
+        """
         query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
 
     def update_product_qnt_by_id(self, product_id, qnt):
-        """The method has two mandatory parameters - product_id, qnt.
+        """
+        The method has two mandatory parameters - product_id, qnt.
         It changes the product quantity according to the value specified in the qnt 
-        for the product with specific product_id"""
+        for the product with specific product_id.
+        """
         query = f"UPDATE products SET quantity = {qnt} WHERE id = {product_id}"
         self.cursor.execute(query)
         self.connection.commit()
 
     def select_product_qnt_by_id(self, product_id):
-        """ The method has a required parameter - product_id.
-        It return the product qnt according to value in the product_id from the products table"""
+        """
+        The method has a required parameter - product_id.
+        It return the product qnt according to value in the product_id from the products table.
+        """
         query = f"SELECT quantity FROM products WHERE id = {product_id}"
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
 
     def insert_product(self, product_id, name, description, qnt):
-        """The method has four mandatory parameters - product_id, name, description, qnt.
-        It inserts/replace data in the table for the columns: id, name, description, quantity."""
+        """
+        The method has four mandatory parameters - product_id, name, description, qnt.
+        It inserts/replace data in the table for the columns: id, name, description, quantity.
+        """
         query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
             VALUES ({product_id}, '{name}', '{description}', {qnt})"
         self.cursor.execute(query)
         self.connection.commit()
 
     def delete_product_by_id(self, product_id):
-        """The method has one required parameter - product_id"
-        It removes the product from the products table"""
+        """
+        The method has one required parameter - product_id"
+        It removes the product from the products table.
+        """
         query = f"DELETE FROM products WHERE id = {product_id}"
         self.cursor.execute(query)
         self.connection.commit()
 
     def get_detailed_orders(self):
-        """The method allows to collect the info about an order
-        using the JOIN command and the orders, customers, products tables"""
+        """
+        The method allows to collect the info about an order
+        using the JOIN command and the orders, customers, products tables.
+        """
         query = "SELECT orders.id, customers.name, products.name, \
                 products.description \
                 FROM orders \
--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -3,15 +3,19 @@ import pytest
 
 @pytest.mark.database
 def test_database_connection(database):
-    """The test is created to execute the test_connection method.
-    It selects sqlite_version and represents the version of the db in the terminal."""
+    """
+    The test is created to execute the test_connection method.
+    It selects sqlite_version and represents the version of the db in the terminal.
+    """
     database.test_connection()
 
 
 @pytest.mark.database
 def test_check_all_users(database):
-    """To execute the object's get_all_users method.
-    The result should be output to the terminal"""
+    """
+    To execute the object's get_all_users method.
+    The result should be output to the terminal.
+    """
     users = database.get_all_users()
 
     print(users)
@@ -19,8 +23,10 @@ def test_check_all_users(database):
 
 @pytest.mark.database
 def test_check_user_sergii(database):
-    """To execute the object method of get_user_address_by_name with name 'Sergii'.
-    The returned data should match the data in the test."""
+    """
+    To execute the object method of get_user_address_by_name with name 'Sergii'.
+    The returned data should match the data in the test.
+    """
     user = database.get_user_address_by_name('Sergii')
 
     assert user[0] == ('Maydan Nezalezhnosti 1', 'Kyiv', '3127', 'Ukraine')
@@ -28,9 +34,11 @@ def test_check_user_sergii(database):
 
 @pytest.mark.database
 def test_product_qnt_update(database):
-    """To execute the method of update_product_qnt_by_id
+    """
+    To execute the method of update_product_qnt_by_id
     with the specific values of product_id and qnt.
-    To check that after updating the data, the qnt of the product(1) is 25"""
+    To check that after updating the data, the qnt of the product(1) is 25.
+    """
     database.update_product_qnt_by_id(1, 25)
     water_qnt = database.select_product_qnt_by_id(1)
 
@@ -39,9 +47,11 @@ def test_product_qnt_update(database):
 
 @pytest.mark.database
 def test_product_insert(database):
-    """To execute the method of insert_product object.
+    """
+    To execute the method of insert_product object.
     To verify that the data has been updated
-    and the product qnt with number 4 is equal to 30"""
+    and the product qnt with number 4 is equal to 30.
+    """
     database.insert_product(4, 'печиво', 'солодке', 30)
     product_qnt = database.select_product_qnt_by_id(4)
     assert product_qnt[0][0] == 30
@@ -49,9 +59,11 @@ def test_product_insert(database):
 
 @pytest.mark.database
 def test_product_delete(database):
-    """The test creates a product (insert_product method) with the specified values.
+    """
+    The test creates a product (insert_product method) with the specified values.
     The product is delited (delete_product_by_id method) from the products table.
-    To check that the number of matching rows is 0."""
+    To check that the number of matching rows is 0.
+    """
     database.insert_product(99, 'тестові', 'дані', 999)
     database.delete_product_by_id(99)
     qnt = database.select_product_qnt_by_id(99)
@@ -61,9 +73,11 @@ def test_product_delete(database):
 
 @pytest.mark.database
 def test_detailed_orders(database):
-    """The test outputs the result of get_detailed_orders method.
+    """
+    The test outputs the result of get_detailed_orders method.
     To check that the number of results is 1.
-    To check structure of data"""
+    To check structure of data.
+    """
     orders = database.get_detailed_orders()
     print("Замовлення", orders)
     
