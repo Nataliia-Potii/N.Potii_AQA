5fd4cb1346439a3ea0e65c75a17402a5301e7038
lorel <loreleysingt@gmail.com>
2023-03-17 05:11:06 +0200

    Task 2: docstrings were changed

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -3,11 +3,15 @@ import pytest
 
 @pytest.mark.check
 def test_change_name(user):
-    """The check that the name is 'Nataliia'."""
+    """
+    The check that the name is 'Nataliia'.
+    """
     assert user.name == 'Nataliia'
 
 
 @pytest.mark.check
 def test_change_second_name(user):
-    """To check that the second name is 'Potii'."""
+    """
+    To check that the second name is 'Potii'.
+    """
     assert user.second_name == 'Potii'
