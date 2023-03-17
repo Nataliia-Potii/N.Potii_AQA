6bafe821eebb1968a85f7e40054e4e068bd60d0e
Nataliia Potii <nataliapotiy@gmail.com>
2023-03-17 06:18:54 +0200

    Fixtures with documentation strings (minor changes)

--- conftest.py
+++ conftest.py
@@ -6,30 +6,41 @@ from modules.ui.page_objects.sign_up_page import SignUpPage
 
 
 class User:
-    """The User class created"""
+    """
+    The User class was created.
+    """
+
     def __init__(self):
-        """The constructor of the User class is created.
-        The name and second_name were set with the default values of None"""
+        """
+        The constructor of the User class is created.
+        The name and second_name were set with the default values of None.
+        """
         self.name = None
         self.second_name = None
 
     def create(self):
-        """The object method sets values to the name and second_name fields"""
+        """
+        The object method sets values to the name and second_name fields.
+        """
         self.name = 'Nataliia'
         self.second_name = 'Potii'
 
     def remove(self):
-        """The object method sets an empty str value to the name and second name fields"""
+        """
+        The object method sets an empty str value to the name and second name fields.
+        """
         self.name = ''
         self.second_name = ''
 
 
 @pytest.fixture
 def user():
-    """The user fixture creates an object of the User class.
+    """
+    The user fixture creates an object of the User class.
     Calls the object's create method.
     Returns the object after calling the object's create method in tests.
-    After executing the test, it calls the object's remove method."""
+    After executing the test, it calls the object's remove method.
+    """
     user = User()
     user.create()
 
@@ -40,8 +51,10 @@ def user():
 
 @pytest.fixture
 def github_api():
-    """The fixture initializes an instance of a GitHub class.
-    Returns the object after calling the object's create method in tests"""
+    """
+    The fixture initializes an instance of a GitHub class.
+    Returns the object after calling the object's create method in tests.
+    """
     api = GitHub()
 
     yield api
@@ -49,17 +62,21 @@ def github_api():
 
 @pytest.fixture
 def database():
-    """The fixture creates an instance of the Database class.
-    Returns the object after calling the object's create method in tests"""
+    """
+    The fixture creates an instance of the Database class.
+    Returns the object after calling the object's create method in tests.
+    """
     database = Database()
     yield database
 
 
 @pytest.fixture
 def sign_in_page():
-    """The fixture creates an instance of the SignInPage class.
+    """
+    The fixture creates an instance of the SignInPage class.
     To call the method go_to to open the SignInPage.
-    Returns the object after calling the object's create method in tests"""
+    Returns the object after calling the object's create method in tests.
+    """
     sign_in_page = SignInPage()
     sign_in_page.go_to()
 
@@ -68,9 +85,11 @@ def sign_in_page():
 
 @pytest.fixture
 def sign_up_page():
-    """The fixture creates an instance of the SignUpPage class.
+    """
+    The fixture creates an instance of the SignUpPage class.
     To call the method go_to to open the SignUpPage.
-    Returns the object after calling the object's create method in tests"""
+    Returns the object after calling the object's create method in tests.
+    """
     sign_up_page = SignUpPage()
     sign_up_page.go_to()
 
