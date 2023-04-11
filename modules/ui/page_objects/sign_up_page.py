b37f17faa918a078128a8306eb4f84d376b13d4f
Nataliia Potii <nataliapotiy@gmail.com>
2023-04-11 10:44:59 +0300

    Changes to Sign up page - reducing methods

--- modules/ui/page_objects/sign_up_page.py
+++ modules/ui/page_objects/sign_up_page.py
@@ -20,44 +20,31 @@ class SignUpPage(BasePage):
         """
         self.driver.get(SignUpPage.URL)
 
-    def click_cookie(self):
+    def check_signup_form(self, email, firstname, lastname, password):
         """
         To find the cookie button and to click.
+        To find the email field and to send the data.
+        To find the firstname field and to send the data.
+        To find the lastname field and to send the data.
+        To find the password field and to send the data.
+        To find the button and to click.
         """
+
         btn_cookie = self.driver.find_element(By.ID, "cookiebotDialogOkButton")
         btn_cookie.click()
 
-    def check_email(self, email):
-        """
-        To find the email field and to send the data.
-        """
         email_elem = self.driver.find_element(By.ID, 'email_id')
         email_elem.send_keys(email)
 
-    def check_firstname(self, firstname):
-        """
-        To find the firstname field and to send the data.
-        """
         first_elem = self.driver.find_element(By.ID, 'firstname_id')
         first_elem.send_keys(firstname)
 
-    def check_lastname(self, lastname):
-        """
-        To find the lastname field and to send the data.
-        """
-        last_elem = self.driver.find_element(By.ID, 'lastname_id')      
+        last_elem = self.driver.find_element(By.ID, 'lastname_id')
         last_elem.send_keys(lastname)
 
-    def check_password(self, password):
-        """
-        To find the password field and to send the data.
-        """
         pass_elem = self.driver.find_element(By.ID, 'password_id')
         pass_elem.send_keys(password)
 
-    def click_btn(self):
-        """
-        To find the button and to click.
-        """
         btn_elem = self.driver.find_element(By.XPATH, "/html/body/div[2]/section/div/div/div[2]/div/form/button")
         btn_elem.click()
+        
--- tests/ui/test_sign_up.py
+++ tests/ui/test_sign_up.py
@@ -2,17 +2,13 @@ import pytest
 
 # The testing that the user cannot create an account with invalid data on Reserved.com.
 
+
 @pytest.mark.ui_up
 def test_check_empty_fields(sign_up_page):
     """
     The test with empty fields.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("")
-    sign_up_page.check_firstname("")
-    sign_up_page.check_lastname("")
-    sign_up_page.check_password("")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("", "", "", "")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -22,12 +18,7 @@ def test_check_registered_email(sign_up_page):
     """
     The test with registered email.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("natatest@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("natatest@gmail.com", "N", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -37,12 +28,7 @@ def test_check_cyrillic_email(sign_up_page):
     """
     The test with cyrillic email.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("тест@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("тест@gmail.com", "N", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -52,12 +38,7 @@ def test_check_incorrect_email(sign_up_page):
     """
     The test with incomplete email.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("@gmail.com", "N", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -67,12 +48,7 @@ def test_check_symbols_email(sign_up_page):
     """
     To check the email with invalid symbols.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("!!!##@^^^.**$")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("!!!##@^^^.**$", "N", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -82,12 +58,7 @@ def test_check_too_short_password(sign_up_page):
     """
     The test with too short password (5 characters).
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("nata3.test@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("11111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("nata3.test@gmail.com", "N", "N", "11111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -97,12 +68,7 @@ def test_check_incorrect_first_name(sign_up_page):
     """
     The test with digit in first name field.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("nata3.test@gmail.com")
-    sign_up_page.check_firstname("1")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("nata3.test@gmail.com", "1", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -112,12 +78,7 @@ def test_check_incorrect_last_name(sign_up_page):
     """
     The test with digit in last name field.
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("nata3.test@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("1")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form("nata3.test@gmail.com", "N", "1", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -127,12 +88,8 @@ def test_check_too_long_first_name(sign_up_page):
     """
     The test with too long first name (31 characters).
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("nata3.test@gmail.com")
-    sign_up_page.check_firstname("Nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")
-    sign_up_page.check_lastname("N")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form(
+        "nata3.test@gmail.com", "Nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn", "N", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
 
@@ -142,11 +99,7 @@ def test_check_too_long_last_name(sign_up_page):
     """
     The test with too long last name (31 characters).
     """
-    sign_up_page.click_cookie()
-    sign_up_page.check_email("nata3.test@gmail.com")
-    sign_up_page.check_firstname("N")
-    sign_up_page.check_lastname("Nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn")
-    sign_up_page.check_password("111111")
-    sign_up_page.click_btn()
+    sign_up_page.check_signup_form(
+        "nata3.test@gmail.com", "N", "Nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn", "111111")
 
     assert sign_up_page.check_title("Reserved.com - купуйте онлайн")
